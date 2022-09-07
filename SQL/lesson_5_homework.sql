02f2a15d3b20938e59dc5cb80f059075a4d0bfb5
Andrewekb <andrewnsk@icloud.com>
2022-09-07 23:22:59 +0500

    Update lesson_5_homework.sql

--- SQL/lesson_5_homework.sql
+++ SQL/lesson_5_homework.sql
@@ -4,6 +4,7 @@
 --task1 (lesson5)
 -- Компьютерная фирма: Сделать view (pages_all_products), в которой будет постраничная разбивка всех продуктов (не более двух продуктов на одной странице). Вывод: все данные из laptop, номер страницы, список всех страниц
 
+
 select page_num, position from
 (select *,
 case when rn % 2 = 0 then rn / 2 else rn / 2 + 1 end  as page_num, 
@@ -14,10 +15,9 @@ from (
 ) a) l
 
 
-
 --task2 (lesson5)
 -- Компьютерная фирма: Сделать view (distribution_by_type), в рамках которого будет процентное соотношение всех товаров по типу устройства. Вывод: производитель, тип, процент (%)
--- в задании нужно вывести maker, сделал таким образом, логика следующая: по производителю внутри типа процентное соотношение моделей, какая доля у производителя конкретного
+
 
 with l_p as
 	(select maker, type, count(m) m  
@@ -40,7 +40,7 @@ from l_p) p
 
 --task3 (lesson5)
 -- Компьютерная фирма: Сделать на базе предыдущенр view график - круговую диаграмму. Пример https://plotly.com/python/histograms/
--- для графика немного скорректирую, диаграмма будет только по типу
+
 
 request = """
 select * from distribution_by_type
@@ -53,12 +53,15 @@ fig.show()
 --task4 (lesson5)
 -- Корабли: Сделать копию таблицы ships (ships_two_words), но название корабля должно состоять из двух слов
 
+
 create table ships_two_words as
 select * from ships s 
 where name like '% %'
 
+
 --task5 (lesson5)
 -- Корабли: Вывести список кораблей, у которых class отсутствует (IS NULL) и название начинается с буквы "S"
+
 
 select * from
 (select name, s.class from ships s
@@ -77,6 +80,7 @@ name like 'S%'
 
 --task6 (lesson5)
 -- Компьютерная фирма: Вывести все принтеры производителя = 'A' со стоимостью выше средней по принтерам производителя = 'C' и три самых дорогих (через оконные функции). Вывести model
+
 
 with p_pr as(
 	select maker, price, p.model m
