a3b139a88fb4ff55375a5eeb6b7b92186cdaf082
Andrewekb <andrewnsk@icloud.com>
2022-11-28 23:07:55 +0500

    total

---
+++ ML/Total/Start.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+
+
+
+
+
+@st.cache(allow_output_mutation=True)
+def get_flights():
+    url = "flights.csv"
+    return pd.read_csv(url)
+
+
+@st.cache(allow_output_mutation=True)
+def get_airport():
+    url = "airports.csv"
+    return pd.read_csv(url)
+
+
+
+
+a = get_flights()
+b = get_airport()
+
+
+all_airport = a.columns
+
+airport = st.text_input('airport')
+
+if airport == 'ALL':
+    ll = a
+else:
+   ll = a.query("ORIGIN_AIRPORT == '{}'".format(airport))
+
+
+#ll = a
+ll['DATE_DAY'] = pd.to_datetime(ll[['YEAR', 'MONTH', 'DAY']])
+ll['HOUR'] = ll['SCHEDULED_DEPARTURE']//100
+ll['MINUTE'] = ll['SCHEDULED_DEPARTURE']%100
+ll['DATE_HOUR'] = pd.to_datetime(ll[['YEAR', 'MONTH', 'DAY', 'HOUR']])
+ll['DEPARTURE_DELAY'].fillna(1, inplace=True)
+ll['PROBABILITY'] = np.where(ll['DEPARTURE_DELAY']>0, 0, 1)
+ll['AIRLINE'] = ll['AIRLINE'].astype(str)
+B = ll.groupby(['ORIGIN_AIRPORT', 'DATE_HOUR'])['DATE_DAY'].count().reset_index().rename(columns={'DATE_DAY': 'COUNT_HOUR'})
+ll = ll.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_HOUR'], right_on = ['ORIGIN_AIRPORT', 'DATE_HOUR'])
+B = ll.groupby(['ORIGIN_AIRPORT', 'DATE_DAY'])['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR': 'COUNT_DAY'})
+ll = ll.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_DAY'], right_on = ['ORIGIN_AIRPORT', 'DATE_DAY'])
+ll = ll.merge(b, how='right', left_on='ORIGIN_AIRPORT', right_on='IATA_CODE')
+ll['AIRLINE'] = ll['AIRLINE'].astype(str)
+
+
+
+fig = pd.DataFrame(ll.isna().sum()).rename(columns={0:'Пропуски'})
+st.bar_chart(fig)
+
+
+
+st.text('Добавлены данные')
+
+
+
+code = '''
+flights['HOUR'] = flights['SCHEDULED_DEPARTURE']//100
+flights['MINUTE'] = flights['SCHEDULED_DEPARTURE']%100
+flights['DATE_DAY'] = pd.to_datetime(flights[['YEAR', 'MONTH', 'DAY']])
+flights['DATE_HOUR'] = pd.to_datetime(flights[['YEAR', 'MONTH', 'DAY', 'HOUR']])
+
+flights['PROBABILITY'] = np.where(flights['DEPARTURE_DELAY']>0, 0, 1)
+
+
+B = flights.groupby(['ORIGIN_AIRPORT', 'DATE_HOUR'])['DATE_DAY'].count().reset_index().rename(columns={'DATE_DAY': 'COUNT_HOUR'})
+flights = flights.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_HOUR'], right_on = ['ORIGIN_AIRPORT', 'DATE_HOUR'])
+B = flights.groupby(['ORIGIN_AIRPORT', 'DATE_DAY'])['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR': 'COUNT_DAY'})
+flights = flights.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_DAY'], right_on = ['ORIGIN_AIRPORT', 'DATE_DAY'])
+ '''
+st.code(code, language='python')
+
+
+st.text("Количество рейсов в день")
+fig = ll.groupby('DATE_DAY')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('DATE_DAY')
+st.line_chart(fig)
+
+ll = ll.loc[ll['DATE_DAY'].between('2015-01-01', '2015-09-30')]
+
+st.text("Количество рейсов в день")
+fig = ll.groupby('DATE_DAY')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('DATE_DAY')
+st.line_chart(fig)
+
+st.text("Количество рейсов в разрезе аэропорта вылета")
+fig = ll.groupby('ORIGIN_AIRPORT')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('ORIGIN_AIRPORT')
+st.bar_chart(fig)
+
+
+st.text("Количество рейсов в различные пункты назначения")
+A = ll.groupby('DESTINATION_AIRPORT')['DATE_HOUR'].count().sort_values(ascending=False).reset_index()
+A = b.merge(A, how='right', left_on='IATA_CODE', right_on='DESTINATION_AIRPORT') 
+
+fig = px.scatter_mapbox(A,
+                    lon = A.LONGITUDE,
+                    lat = A.LATITUDE,
+                    zoom = 2,
+                    hover_name="IATA_CODE",
+                    size = A.DATE_HOUR,
+                    color = A.DATE_HOUR
+                    #width = 800,
+                    #height = 600,
+                                )
+fig.update_layout(mapbox_style='open-street-map')
+fig.show()
+st.plotly_chart(fig, use_container_width=True)
+
+st.text("Количество рейсов для авиакомпаний")
+fig = ll.groupby('AIRLINE')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('AIRLINE')
+st.bar_chart(fig)
+
+st.text("Количество рейсов в разрезе года по месяцам")
+fig = ll.groupby('MONTH')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('MONTH')
+st.line_chart(fig)
+st.bar_chart(fig)
+
+st.text("Количество рейсов в разрезе года по дням недели")
+fig = ll.groupby('DAY_OF_WEEK')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('DAY_OF_WEEK')
+st.line_chart(fig)
+st.bar_chart(fig)
+
+st.text("Количество рейсов в разрезе года в зависимости от часа вылета")
+fig = ll.groupby('HOUR')['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR':'Количество рейсов'}).set_index('HOUR')
+st.line_chart(fig)
+st.bar_chart(fig)
+
+st.text("Количество вылетов, положительная вероятность прилета")
+A = ll.groupby('DATE_DAY')['DATE_HOUR'].count().reset_index()
+A['DATE_HOUR'] = A['DATE_HOUR']/A['DATE_HOUR'].max()
+B = ll.groupby('DATE_DAY')['PROBABILITY'].mean().reset_index()
+fig = B.merge(A, how='right', left_on='DATE_DAY', right_on='DATE_DAY').set_index('DATE_DAY')
+fig = fig.rename(columns={'DATE_HOUR':'Рейсы', 'PROBABILITY':'Вероятность'})
+st.line_chart(fig)
+
+
+st.text('Количество отмененных рейсов (причина)') 
+
+fig = ll.groupby('CANCELLATION_REASON')['DATE_DAY'].count()
+st.bar_chart(fig)
+
+
+
+st.text('Средняя вероятность прилета в срок, количество вылетов, задержка из-за погоды') 
+
+A = ll.groupby('DATE_DAY')['DATE_HOUR'].count().reset_index()
+A['DATE_HOUR'] = A['DATE_HOUR']/A['DATE_HOUR'].max()
+B = ll.groupby('DATE_DAY')['PROBABILITY'].mean().reset_index()
+df = B.merge(A, how='right', left_on='DATE_DAY', right_on='DATE_DAY').set_index('DATE_DAY')
+B = ll.groupby('DATE_DAY')['WEATHER_DELAY'].count().reset_index()
+B['WEATHER_DELAY'] = B['WEATHER_DELAY']/B['WEATHER_DELAY'].max()
+fig = df.merge(B, how='right', left_on='DATE_DAY', right_on='DATE_DAY').set_index('DATE_DAY')
+fig = fig.rename(columns={'DATE_HOUR':'Рейсы', 'PROBABILITY':'Вероятность', 'WEATHER_DELAY':'Задержка погода'})
+st.line_chart(fig)
+
+
+
---
+++ ML/Total/pages/Classifier.py
@@ -0,0 +1,328 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.metrics import mean_squared_error
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+st.text('Используемые библиотеки')
+code = '''
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.metrics import mean_squared_error
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler
+ '''
+st.code(code, language='python')
+
+
+@st.cache(allow_output_mutation=True)
+def get_flights():
+    url = "flights.csv"
+    return pd.read_csv(url)
+
+
+@st.cache(allow_output_mutation=True)
+def get_airport():
+    url = "airports.csv"
+    return pd.read_csv(url)
+
+
+
+
+
+a = get_flights()
+b = get_airport()
+
+
+all_airport = a.columns
+
+airport = st.text_input('airport')
+
+if airport == 'ALL':
+    ll = a
+else:
+   ll = a.query("ORIGIN_AIRPORT == '{}'".format(airport))
+
+
+st.text('Общее количество возможных направлений {}'.format(ll.DESTINATION_AIRPORT.nunique()))
+
+
+#ll = a
+ll['DATE_DAY'] = pd.to_datetime(ll[['YEAR', 'MONTH', 'DAY']])
+ll['HOUR'] = ll['SCHEDULED_DEPARTURE']//100
+ll['MINUTE'] = ll['SCHEDULED_DEPARTURE']%100
+ll['DATE_HOUR'] = pd.to_datetime(ll[['YEAR', 'MONTH', 'DAY', 'HOUR']])
+ll['DEPARTURE_DELAY'].fillna(1, inplace=True)
+ll['PROBABILITY'] = np.where(ll['DEPARTURE_DELAY']>0, 0, 1)
+ll['AIRLINE'] = ll['AIRLINE'].astype(str)
+B = ll.groupby(['ORIGIN_AIRPORT', 'DATE_HOUR'])['DATE_DAY'].count().reset_index().rename(columns={'DATE_DAY': 'COUNT_HOUR'})
+ll = ll.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_HOUR'], right_on = ['ORIGIN_AIRPORT', 'DATE_HOUR'])
+B = ll.groupby(['ORIGIN_AIRPORT', 'DATE_DAY'])['DATE_HOUR'].count().reset_index().rename(columns={'DATE_HOUR': 'COUNT_DAY'})
+ll = ll.merge(B, how='left', left_on=['ORIGIN_AIRPORT', 'DATE_DAY'], right_on = ['ORIGIN_AIRPORT', 'DATE_DAY'])
+ll = ll.merge(b, how='right', left_on='ORIGIN_AIRPORT', right_on='IATA_CODE')
+ll['AIRLINE'] = ll['AIRLINE'].astype(str)
+ll = ll.loc[ll['DATE_DAY'].between('2015-01-01', '2015-09-30')]
+
+
+
+
+
+
+st.text('Данные для анализа')
+
+
+
+
+
+code = '''
+        [['MONTH', 'DAY_OF_WEEK', 'AIRLINE','SCHEDULED_TIME',
+       'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT',
+       'SCHEDULED_DEPARTURE', 'DISTANCE',
+       'ARRIVAL_DELAY', 'HOUR', 'MINUTE',
+       'DATE_HOUR', 'PROBABILITY', 'COUNT_HOUR', 'COUNT_DAY']]
+ '''
+st.code(code, language='python')
+
+
+
+
+C = ll[['MONTH', 'DAY_OF_WEEK', 'AIRLINE','SCHEDULED_TIME',
+       'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT',
+       'SCHEDULED_DEPARTURE', 'DISTANCE',
+       'ARRIVAL_DELAY', 'HOUR', 'MINUTE',
+       'DATE_HOUR', 'PROBABILITY', 'COUNT_HOUR', 'COUNT_DAY']]
+
+
+options = st.multiselect(
+    'Выбрать необходимые показатели для построения heatmap',
+    list(C.columns))
+
+fig = pd.DataFrame(C.isna().sum()).rename(columns={0:'Пропуски'})
+st.bar_chart(fig)
+
+
+C.dropna(inplace=True)
+
+
+code = '''
+C.dropna(inplace=True)
+ '''
+st.code(code, language='python')
+
+
+
+
+if st.button('Попарная корреляция/ковариация'):
+    
+    A =  C[options]
+    A.set_index('DATE_HOUR', inplace=True)
+    le = LabelEncoder()
+    for column in A.columns:
+        if A.dtypes['{}'.format(column)] == 'object':
+            A['{}'.format(column)] = le.fit_transform(A['{}'.format(column)])
+
+    fig, ax = plt.subplots()  
+    colormap = plt.cm.viridis
+    sns.heatmap(A.corr(), ax=ax, cmap=colormap)
+    st.write(fig)
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rescaledX = scaler.fit_transform(A)
+    col = A.columns
+    skalre_df = pd.DataFrame(rescaledX, columns=col )
+
+
+    code = '''
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rescaledX = scaler.fit_transform(A)
+    col = A.columns
+    skalre_df = pd.DataFrame(rescaledX, columns=col )
+    '''
+    st.code(code, language='python')
+
+    fig, ax = plt.subplots()  
+    colormap = plt.cm.viridis
+    sns.heatmap(skalre_df.cov(), ax=ax, cmap=colormap)
+    st.write(fig)
+
+
+if st.button('go!'):
+    a = 1
+    C =  C[options]
+    st.text(C.columns)
+
+    a_list = []
+    le = LabelEncoder()
+    for column in C.columns:
+        if C.dtypes['{}'.format(column)] == 'object':
+            a_list.append(column)
+            C['{}'.format(column)] = le.fit_transform(C['{}'.format(column)])
+
+    
+    code = '''
+    Меняю строковые данные числовыми с помощью LabelEncoder
+    a_list = []
+    le = LabelEncoder()
+    for column in C.columns:
+        if C.dtypes['{}'.format(column)] == 'object':
+            a_list.append(column)
+            C['{}'.format(column)] = le.fit_transform(C['{}'.format(column)])
+ '''
+    st.code(code, language='python')
+
+
+    C.set_index('DATE_HOUR', inplace=True)
+
+
+
+
+
+    fig = px.box(C['ARRIVAL_DELAY'], title='Распределение задержки вылета, минуты')
+    fig.show()
+    st.plotly_chart(fig, use_container_width=True)
+
+
+    #удаляю выбросы - 1,5 межквантильных размаха из колонки с задержкой прилета
+    Q1 = C['ARRIVAL_DELAY'].quantile(0.25)
+    Q3 = C['ARRIVAL_DELAY'].quantile(0.75)
+    IQR = Q3 - Q1
+    a = C[(C['ARRIVAL_DELAY'] < Q1-1.5*IQR ) | (C['ARRIVAL_DELAY'] > Q3+1.5*IQR)]['ARRIVAL_DELAY'].reset_index()
+
+
+    code = '''
+    Удаляю выбросы
+    Q1 = C['ARRIVAL_DELAY'].quantile(0.25)
+    Q3 = C['ARRIVAL_DELAY'].quantile(0.75)
+    IQR = Q3 - Q1
+    a = C[(C['ARRIVAL_DELAY'] < Q1-1.5*IQR ) | (C['ARRIVAL_DELAY'] > Q3+1.5*IQR)]['ARRIVAL_DELAY'].reset_index()
+    '''
+    st.code(code, language='python')
+
+
+    
+    
+    #список с исключениями
+    a = list(a.ARRIVAL_DELAY)
+    #исключаю выбросы
+    C = C.query("ARRIVAL_DELAY != @a")
+
+
+    fig = px.box(C['ARRIVAL_DELAY'], title='Распределение задержки вылета, минуты')
+    fig.show()
+    st.plotly_chart(fig, use_container_width=True)
+
+
+    C.drop(['PROBABILITY'], axis=1, inplace=True)
+
+    #формирую выборки - обучающую и тестовую
+    X_train,X_test, y_train, y_test = train_test_split(C.drop('ARRIVAL_DELAY',axis=1),
+                                                        C.ARRIVAL_DELAY,
+                                                        shuffle = False,# временной ряд, отключаем перемешивание
+                                                        test_size = 0.25)
+
+    
+
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    print('Ошибка на обучающей выборке', (mean_squared_error(y_train, dtc.predict(X_train)))**0.5)
+    pred = dtc.predict(X_test)
+    print('Ошибка на тесте', (mean_squared_error(y_test, pred))**0.5)
+
+    code = '''
+    Работа модели
+    X_train,X_test, y_train, y_test = train_test_split(C.drop('ARRIVAL_DELAY',axis=1),
+                                                        C.ARRIVAL_DELAY,
+                                                        shuffle = False,# временной ряд, отключаем перемешивание
+                                                        test_size = 0.25)
+
+    
+
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    print('Ошибка на обучающей выборке', (mean_squared_error(y_train, dtc.predict(X_train)))**0.5)
+    pred = dtc.predict(X_test)
+    print('Ошибка на тесте', (mean_squared_error(y_test, pred))**0.5)
+    '''
+    st.code(code, language='python')
+
+
+    y_test = y_test.reset_index()
+    y_test['PRED'] = list(pred)
+    y_test = y_test.set_index('DATE_HOUR')
+    X_test['ARRIVAL_DELAY'] = y_test['ARRIVAL_DELAY']
+    X_test['PRED'] = y_test['PRED']
+
+    for column in a_list:
+        X_test['{}'.format(column)] = le.inverse_transform(X_test['{}'.format(column)])
+
+
+    code = '''
+    Декодирую данные в строки
+    for column in a_list:
+        X_test['{}'.format(column)] = le.inverse_transform(X_test['{}'.format(column)])
+    '''
+    st.code(code, language='python')
+
+
+
+    X_test['PROBABILITY'] = np.where(X_test['ARRIVAL_DELAY']>0, 0, 1)
+    X_test['PROBABILITY_PRED'] = np.where(X_test['PRED']>0, 0, 1)
+    X_test = X_test.reset_index()
+    X_test['MSE'] = (X_test['PRED'] - X_test['ARRIVAL_DELAY'])**2
+    A = X_test.groupby('DESTINATION_AIRPORT')['ARRIVAL_DELAY'].count().reset_index().rename(columns={'ARRIVAL_DELAY':'COUNT'})
+    X_test = X_test.merge(A, how='left', left_on='DESTINATION_AIRPORT', right_on='DESTINATION_AIRPORT' )
+    
+
+    X_test = X_test.groupby('DESTINATION_AIRPORT').mean()
+    X_test['RMSE'] = round(np.sqrt(X_test['MSE']),0)
+    X_test = X_test.query("COUNT > 60")[['ARRIVAL_DELAY', 'PRED',  'PROBABILITY', 'PROBABILITY_PRED', 'RMSE', 'COUNT']]
+  
+    X_test = X_test.sort_values(by=['RMSE','ARRIVAL_DELAY'],ascending=[True, True])
+    st.dataframe(X_test)
+
+    fig = px.bar(X_test,y=['PROBABILITY', 'PROBABILITY_PRED'], barmode = 'group', title='Сравнение вероятностей положительного исхода')
+    fig.show()
+    st.plotly_chart(fig, use_container_width=True)
+
+
+    fig = px.bar(X_test,y=['ARRIVAL_DELAY', 'PRED'], barmode = 'group', title='Сравнение задержки вылета')
+    fig.show()
+    st.plotly_chart(fig, use_container_width=True)
+
+    X_test = X_test.reset_index().head(3)
+
+
+    t = {'DESTINATION_AIRPORT':'{}'.format(airport), 'ARRIVAL_DELAY':0, 'PRED':0,  'PROBABILITY':0, 'PROBABILITY_PRED':0, 'RMSE':50, 'COUNT':0}
+    X_test = X_test.append(t, ignore_index=True)
+
+    X_test = X_test.reset_index().merge(b, how='left', left_on='DESTINATION_AIRPORT', right_on='IATA_CODE')
+
+
+    fig = px.scatter_mapbox(X_test,
+                    lon = X_test.LONGITUDE,
+                    lat = X_test.LATITUDE,
+                    zoom = 2,
+                    color = X_test.RMSE,
+                    size = X_test.RMSE ,
+                    hover_name="DESTINATION_AIRPORT",
+                    width = 800,
+                    height = 600,
+                    title='Лучшие направления вылета'
+                                )
+    fig.update_layout(mapbox_style='open-street-map')
+    fig.show()
+    st.plotly_chart(fig)
